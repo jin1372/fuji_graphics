902a387cdf3abf3555166c64486c7c1fe515fcca
test3 <chlcjfwls1@naver.com>
2023-05-12 17:11:59 +0900

    Third Commit

--- TestCode/TestCode/TestCode.cpp
+++ TestCode/TestCode/TestCode.cpp
@@ -5,12 +5,11 @@ using namespace std;
 int main( void )
 {
 	float a = 3.3f;
-	float f = 2.3f;
-
 
 	cout << " 나는 테스트 코드입니다. " << endl;
 	cout << " 추가된 내용의 코드입니다. " << endl;
 
+	cout << a + a << endl;
 
 	return 0;
 }
